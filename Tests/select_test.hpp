e81853183e1881de17534653875b1c368dea7f4b
xshen053 <sehnxiaxi26@gmail.com>
2022-03-10 12:32:34 -0800

    change

--- Tests/select_test.hpp
+++ Tests/select_test.hpp
@@ -32,5 +32,48 @@ TEST(SelectTest, Orderbyduedate){
 
 }
 
+TEST(SelectTest, Orderbypriority){
+    vector<Base*> data;
+    Base* a = new Task("milk", "high", "whoe milk", "11/03/2022", "personal");
+
+    Base* b = new Task("egg", "medium", "chicken eggs", "09/03/2022", "work");
+
+    Base* c = new Task("bread", "low", "brown bread", "10/03/2022", "study");
+
+    data.push_back(a);
+
+    data.push_back(b);
+
+    data.push_back(c);
+    Select* strategy;
+    strategy = new Select_ByPriority(new Select_GetList(data));
+    vector<Base*> out;
+    out = strategy->getData();
+    EXPECT_EQ(out.at(0)->getName(), "milk");
+    
+}
+
+TEST(SelectTest, OrderbyPriorityAndDuedate){
+    vector<Base*> data;
+    Base* a = new Task("milk", "high", "whoe milk", "11/03/2022", "personal");
+
+    Base* b = new Task("egg", "high", "chicken eggs", "09/03/2022", "work");
+
+    Base* c = new Task("bread", "low", "brown bread", "10/03/2022", "study");
+
+    data.push_back(a);
+
+    data.push_back(b);
+
+    data.push_back(c);
+    Select* strategy;
+    strategy = new Select_ByDueDate(new Select_ByPriority(new Select_GetList(data)));
+    vector<Base*> out;
+    out = strategy->getData();
+    EXPECT_EQ(out.at(0)->getName(), "egg");
+
+}
+
+
 
 #endif
